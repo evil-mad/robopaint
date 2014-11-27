c14a248d0d562c732d059bb7e61012d4476818c0
rogerfachini <r3fachini@live.com>
2014-11-27 10:21:28 -0800

    Colorset translations WORK!!!!!!
    After a VERY busy week of me not having time to work on robopaint, I
    finally got colorset translations DONE and WORKING (for the most part)!
    I was only able to port the colorsets over to English, but this gives
    our translation gurus a template to work off of. Anyways, here's a
    feature list:
    - Added i18n folders and en-US translation files for respective
    colorsets
    - Modified colorset definition files to move all text elements to i18n
    files.
    - Modified colorset parser to get text from i18n file and reference a
    non-keyed color array.

--- resources/main.js
+++ resources/main.js
@@ -550,19 +550,24 @@ function getColorsets() {
       continue;
     }
 
-    // Move through all colorsets in file
+     // Move through all colorsets in file
     for(var s in fileSets) {
       var c = fileSets[s];
 
       try {
         // Add pure white to the end of the color set for auto-color
-        c.colors.push({'White': '#FFFFFF'});
+        //TODO: Figure out if commenting this out breaks anything!
+        //EDIT: Yes, it does.
+        //TODO: Fix the broken thing!
+        c.colors.push('#FFFFFF');
 
         // Process Colors to avoid re-processing later
         var colorsOut = [];
+        var iMod = 8*s
         for (var i in c.colors){
-          var name = Object.keys(c.colors[i])[0];
-          var h = c.colors[i][name];
+          var idx = parseInt(i)+iMod;
+          var name = "colorsets." + set + ".color" + idx;
+          var h = c.colors[i];
           var r = robopaint.utils.colorStringToArray(h);
           name = robopaint.t(name); //Translate name if it is part of i18n
           colorsOut.push({
@@ -573,19 +578,20 @@ function getColorsets() {
               HSL: robopaint.utils.rgbToHSL(r),
               YUV: robopaint.utils.rgbToYUV(r)
             }
+
           });
         }
       } catch(e) {
-        // Silently fail on bad parse!
+        console.error("Parse error on colorset: " + s, e);
         continue;
       }
 
       robopaint.statedata.colorsets[c.styles.baseClass] = {
-        name: robopaint.t(c.name),
-        type: robopaint.t(c.type),
+        name: robopaint.t("colorsets." + set + ".info" + s + ".name"),
+        type: robopaint.t("colorsets." + set + ".info" + s + ".type"),
         weight: parseInt(c.weight),
-        description: robopaint.t(c.description),
-        media: robopaint.t(c.media),
+        description: robopaint.t("colorsets." + set + ".info" + s + ".description"),
+        media: robopaint.t("colorsets." + set + ".info" + s + ".media"),
         baseClass: c.styles.baseClass,
         colors: colorsOut,
         stylesheet: $('<link>').attr({rel: 'stylesheet', href: setDir + c.styles.src}),
@@ -795,6 +801,9 @@ function translatePage() {
 
       // Add the language to the resource list.
       resources[data['_meta'].target] = { translation: data};
+      //Create empty colorset key
+      resources[data['_meta'].target].translation['colorsets'] = {};
+
       i += 1;
     } catch(e) {
       console.error('Bad language file:' + file, e);
@@ -814,7 +823,7 @@ function translatePage() {
           fs.readdirSync(fullPath).forEach(function(file) {
               var data = JSON.parse(fs.readFileSync(fullPath + file , 'utf8'));
               //Add the data to the global i18n translation set
-              resources[data['_meta'].target]['translation'][folder] = data;
+              resources[data['_meta'].target].translation['colorsets'][folder] = data;
       });
       }
     } catch(e) {
@@ -874,6 +883,8 @@ function updateLang() {
       var bt = robopaint.currentBot.type != "watercolorbot" ? ' - ' + robopaint.currentBot.name : '';
       $('span.version').text('('+ robopaint.t('nav.toolbar.version') + gui.App.manifest.version + ')' + bt);
     });
+  // Report language switch to the console
+  console.info("Language Switched to: " + robopaint.settings.lang);
 
   // Initalize/reset Tooltips
   initToolTips();
@@ -881,8 +892,7 @@ function updateLang() {
   // Reload colorsets
   getColorsets();
 
-  // Report language switch to the console
-  console.info("Language Switched to: " + robopaint.settings.lang);
+
 
   // Apply bolding to details text
   $('aside').each(function(){
