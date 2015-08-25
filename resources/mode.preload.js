bdec7245163390547e8c1754ade1bd8e39c0ae4b
techninja <james@tn42.com>
2015-08-25 15:08:44 -0700

    WIP: Initial working (sort of) mode translation

--- resources/mode.preload.js
+++ resources/mode.preload.js
@@ -10,14 +10,14 @@
 var remote = require('remote');
 var path = require('path');
 var app = remote.require('app');
+var fs = require('fs-plus');
 var appPath = app.getAppPath();
-var i18n = require('i18n-client');
-window.i18n = i18n;
+var i18n = window.i18n = require('i18next-client');
 var $ = require('jquery');
 
 // Get our mode path, find the mode's package.json, and load it.
 var modePath = path.parse(window.location.pathname);
-var mode = require(path.join(modePath.dir, 'package.json'));
+var mode = window.mode = require(path.join(modePath.dir, 'package.json'));
 mode.path = modePath;
 
 
@@ -74,4 +74,137 @@ function insertScript(src) {
   return script;
 }
 
-console.log('RobPaint Mode APIs Preloaded...');
+/**
+ * Load language resources for this mode and RP common
+ */
+function i18nInit() {
+  var res = {};
+  var langCode;
+
+  console.log('Loading languages...');
+
+  // Iterate over language files for main RP i18n folder (gives mode access to
+  // common strings).
+  var fullPath = path.join(appPath, 'resources', '_i18n');
+  fs.readdirSync(fullPath).forEach(function(file) {
+    try {
+      //  Add the data to the resource translation object
+      var data = require(path.join(fullPath, file));
+      langCode = data._meta.target;
+
+      // Init empty resource language targets
+      if (!res[langCode]) {
+        res[langCode] = { translation: {} };
+      }
+
+      res[langCode].translation = data;
+    } catch(e) {
+      console.error('Bad language file:' + path.join(fullPath, file), e);
+    }
+  });
+
+  // Iterate over language files in mode's i18n folder
+  fullPath = path.join(mode.path.dir, '_i18n');
+  fs.readdirSync(fullPath).forEach(function(file) {
+    if (file.indexOf('.map.json') === -1) { // Don't use translation maps.
+      try {
+        //  Add the data to the resource translation object
+        var data = require(path.join(fullPath, file));
+        langCode = data._meta.target;
+
+        // Init empty resource language targets
+        if (!res[langCode].translation.modes) {
+          res[langCode].translation.modes = {};
+        }
+
+        res[langCode].translation.modes[mode.name] = data;
+      } catch(e) {
+        console.error('Bad language file:' + path.join(fullPath, file), e);
+      }
+    }
+  });
+
+  i18n.init({
+    resStore: res,
+    ns: 'translation',
+    fallbackLng: 'en-US',
+    lng: localStorage['robopaint-lang']
+  });
+
+  // On jQuery load trigger, run the initial translation
+  $(function(){ translateMode(); })
+}
+
+/**
+ * Translate a mode, in either native or DOM map format. Will also trigger
+ * translateComplete() function on window.mode object if it exists.
+ */
+function translateMode() {
+  i18n.setLng(localStorage['robopaint-lang']);
+  // DOM Map or native parsing?
+  if (mode.i18n == 'dom') {
+    var domFile = 'resources/modes/' + mode.name + '/_i18n/' + mode.name + '.map.json';
+    try {
+      var mappings = require(domFile).map;
+      for (var selector in mappings) {
+        var $elements = $(selector, $subwindow.contents());
+
+        if ($elements.length === 0) {
+          console.debug("TranslationDOM Map selector not found:", selector);
+        }
+
+        // When creating DOM map and i18n for non-native modes, it helps to know
+        // which ones are done, and which aren't!
+        var debugExtra = ""; //"XXX";
+
+        // Replace text or specific attributes?
+        var i18nKey = mappings[selector];
+        if (typeof i18nKey === 'string') {
+          // Can't use .text() as it will replace child nodes!
+          $elements.each(function(){ // Just in case we select multiple elements.
+            $(this)
+              .contents()
+              .filter(function(){ return this.nodeType == 3; })
+              .first()
+              .replaceWith(robopaint.t(i18nKey) + debugExtra);
+          });
+        } else if (typeof i18nKey === 'object') {
+          for (var attr in i18nKey) {
+            $elements.each(function(){ // Just in case we select multiple elements.
+              if (attr === 'text') {
+                $(this)
+                  .contents()
+                  .filter(function(){ return this.nodeType == 3; })
+                  .first()
+                  .replaceWith(robopaint.t(i18nKey.text) + debugExtra);
+              } else {
+                $(this).attr(attr, robopaint.t(i18nKey[attr]) + debugExtra);
+              }
+            });
+          }
+        }
+     }
+
+    } catch(e) {
+      console.error('Bad DOM location file:' + domFile, e);
+    }
+  } else { // Native i18n parsing! (much simpler)
+    // Quick fix for non-reactive re-translate for modes
+    $('[data-i18n=""]').each(function() {
+      var $node = $(this);
+      if ($node.text().indexOf('.') > -1 && $node.attr('data-i18n') == "") {
+        $node.attr('data-i18n', $node.text());
+      }
+    });
+    i18n.translateObject(window.document.body);
+  }
+
+  // If this mode implements a translateComplete callback, call it.
+  if (typeof mode.translateComplete === 'function') {
+    mode.translateComplete();
+  }
+}
+
+i18nInit();
+
+console.log('RobPaint Mode APIs Preloaded & Ready!');
