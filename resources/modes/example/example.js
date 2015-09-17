c7e4d48b42adb689347e3e03fca76db3196afbe7
techninja <james@tn42.com>
2015-09-17 02:58:51 -0700

    Initial conversion of example mode to new mode system

--- resources/modes/example/example.js
+++ resources/modes/example/example.js
@@ -1,102 +1,124 @@
 /**
- * @file Holds all RoboPaint example mode text renderer initialization code
+ * @file Holds all RoboPaint example mode text renderer initialization code.
+ *   @see the mode readme for all the goodies that modes come with. Also,
+ *      any robopaint dependencies you add to the package JSON will be available
+ *      as well, like jQuery $ and underscore _.
  */
+"use strict";
 
-robopaintRequire(['superdom', 'hersheytext', 'svgshared', 'wcb', 'commander'],
-function($, robopaint, cncserver) {
+var actualPen = {}; // Hold onto the latest actualPen object from updates.
+var buffer = {};
+var canvas = rpRequire('canvas');
+var hershey = require('hersheytext');
+var textHasRendered = false; // First run boolean
 
-  // We don't ever check visibility/overlap for this mode because the
-  // text output is fully linear and only lines
-  cncserver.config.checkVisibility = false;
-
-// On page load complete...
-$(function() {
-  // Fit the canvas and other controls to the screen size
-  responsiveResize();
-  setTimeout(responsiveResize, 500);
-  $(window).resize(responsiveResize);
-  var textHasRendered = false; // First run boolean
-
-  // Populate font choices
-  for (var id in cncserver.fonts) {
-    $('<option>').text(cncserver.fonts[id].name).val(id).appendTo('#fontselect');
-  }
-
-  // Externally accessible bind controlls trigger for robopaint.mode.svg to call
-  window.bindControls = function() {
-    // Bind trigger for font selection
-    $('#fontselect').change(function(){
-      textHasRendered = true;
-      $('#textexample').remove(); // Kill the old one (if any)
-
-      // Render some text into the SVG area with it
-      cncserver.renderText($('#fonttext').val(), {
-        font: cncserver.fonts[$(this).val()],
-        pos: {x: 0, y: 0},
-        scale: parseInt($('#scale').val()) / 100,
-        charWidth: parseInt($('#charwidth').val()),
-        wrapWidth: parseInt($('#wrap').val()),
-        centerWidth: parseInt($('#hcenter').val()),
-        centerHeight: parseInt($('#vcenter').val()),
-        target: '#target',
-        id: 'textexample'
-      });
-    }).val('futural'); // Set default font
-
-    // Re-render on keypress/change
-    $('input').on('input change', function(e){
-      $('#fontselect').change();
-    });
-
-    // Bind save functionality
-    $('#save').click(function() {
-      cncserver.canvas.saveSVG($('#export').html());
-    });
-
-    // Bind print functionality
-    $('#print').click(function() {
-      $('#save').click();
-      robopaint.switchMode('print');
-    });
-  }
-
-  // Externally accessible event for when the mode is translated (can be called
-  // multiple times during a session, for every language change)
-  window.translateComplete = function() {
-    // Trigger initial run if no text and translate is done
-    if (!textHasRendered) $('#fonttext').change();
-  };
-
-  // Externalize for remote triggering
-  window.responsiveResize = responsiveResize;
-  function responsiveResize(){
-     var w = $(window).width();
-    var h = $(window).height();
-
-    // These value should be static, set originally from central canvas config
-    var mainOffset = {
+mode.pageInitReady = function () {
+  // Initialize the paper.js canvas with wrapper margin and other settings.
+  canvas.domInit({
+    replace: '#paper-placeholder', // jQuery selecter of element to replace
+    paperScriptFile: 'example.ps.js', // The main PaperScript file to load
+    wrapperMargin: {
       top: 30,
       left: 30,
-      bottom: 40,
-      right: $('#control').width() + 50
-    };
+      right: 265,
+      bottom: 40
+    },
 
-    // Calculate scale for both width and height...
-    var scale = {
-      x: (w - (mainOffset.left + mainOffset.right)) / cncserver.canvas.width,
-      y: (h - (mainOffset.top + mainOffset.bottom)) / cncserver.canvas.height
-    }
+    // Called when PaperScript init is complete, requires
+    // canvas.paperInit(paper) to be called in this modes paperscript file.
+    // Don't forget that!
+    loadedCallback: paperLoadedInit
+  });
+}
 
-    // ...use the smaller of the two
-    cncserver.canvas.scale = scale.x < scale.y ? scale.x : scale.y;
+// Callback that tells us that our Paper.js canvas is ready!
+function paperLoadedInit() {
+  console.log('Paper ready!');
 
-    $('#scale-container') // Actually do the scaling
-      .css('-webkit-transform', 'scale(' + cncserver.canvas.scale + ')');
+  // Set center adjusters based on size of canvas
+  $('#hcenter').attr({
+    value: 0,
+    min: -(robopaint.canvas.width / 2),
+    max: robopaint.canvas.width / 2
+  });
 
-    cncserver.canvas.offset.left = mainOffset.left+1;
-    cncserver.canvas.offset.top = mainOffset.top+1;
+  $('#vcenter').attr({
+    value: 0,
+    min: -(robopaint.canvas.height / 2),
+    max: robopaint.canvas.height / 2
+  });
+
+  $(window).resize();
+
+  // Use mode settings management on all "managed" class items. This
+  // saves/loads settings from/into the elements on change/init.
+  mode.settings.$manage('.managed');
+
+  // With Paper ready, send a single up to fill values for buffer & pen.
+  mode.run('up');
+}
+
+// Bind all controls (happens before pageInitReady, @see mode.preload.js)
+mode.bindControls = function() {
+  // Populate font choices
+  _.each(hershey.fonts, function(font, id){
+    $('<option>').text(font.name).val(id).appendTo('#fontselect');
+  });
+
+  // Bind trigger for font selection
+  $('#fontselect').change(function(){
+    textHasRendered = true;
+
+    // Render some text into the SVG area with it
+    paper.renderText($('#fonttext').val(), {
+      layer: paper.canvas.actionLayer,
+      font: $(this).val(),
+      pos: {x: 0, y: 0},
+      scale: parseInt($('#scale').val()) / 100,
+      spaceWidth: parseInt($('#spacewidth').val()),
+      charSpacing: parseFloat($('#charspacing').val() / 4),
+      wrapWidth: parseInt($('#wrap').val()),
+      lineHeight:parseFloat($('#lineheight').val() / 4),
+      hCenter: parseInt($('#hcenter').val()),
+      vCenter: parseInt($('#vcenter').val()),
+      textAlign: $('#textalign input:checked').val()
+    });
+  }).val('futural'); // Set default font
+
+  // Re-render on keypress/change
+  $('input').on('input change', function(e){
+    $('#fontselect').change();
+  });
+
+  // Bind save functionality
+  $('#save').click(function() {
+    //cncserver.canvas.saveSVG($('#export').html());
+  });
+
+  // Bind print functionality
+  $('#print').click(function() {
+    paper.utils.ungroupAllGroups(paper.canvas.actionLayer);
+    paper.utils.autoPaint(paper.canvas.actionLayer);
+    paper.canvas.actionLayer.removeChildren();
+    $('#fontselect').change();
+  });
+}
+
+// Actual pen update event
+mode.onPenUpdate = function(botPen){
+  paper.canvas.drawPoint.move(botPen.absCoord, botPen.lastDuration);
+  actualPen = $.extend({}, botPen);
+
+  // Update button text/state
+  // TODO: change implement type <brush> based on actual implement selected!
+  /*var key = 'common.action.brush.raise';
+  if (actualPen.state === "up" || actualPen.state === 0){
+    key = 'common.action.brush.lower';
   }
+  $('#pen').text(t(key));*/
+}
 
-}); // End Page load complete
-
-}); // End RequireJS init
+// An abbreviated buffer update event, contains paused/not paused & length.
+mode.onBufferUpdate = function(b) {
+  buffer = b;
+}
