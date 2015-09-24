bf42a6a46aa2aa0ce29671ffcaa547c2d58dc35a
techninja <james@tn42.com>
2015-09-24 00:17:43 -0700

    Have the app actually close on mac for now till we get menus

--- resources/scripts/root/window-main.js
+++ resources/scripts/root/window-main.js
@@ -30,9 +30,10 @@ function windowInit() {
   app.on('window-all-closed', function() {
     // On OSX it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
+    // TODO: We're disabling this till we can make menus for making windows!
+    /*if (process.platform != 'darwin') {
       app.quit();
-    }
+    }*/
   });
 
   // This method will be called when Electron has done everything
